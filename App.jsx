c2a1011a21436004c69de0a5839d7aa87e89a354
Yaron Ben Yosef. <Yaronby8@gmail.com>
2025-07-10 13:23:01 +0300

    העלאה ראשונה של האתר

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <img
+        src="https://i.imgur.com/jRlf3ht.jpg"
+        alt="הזמנה לבר מצווה של אראל"
+        className="invite-image"
+      />
+      <h3>נא אשרו את הגעתכם:</h3>
+      <p>לחץ על אחד הכפתורים כדי לאשר או לבטל הגעה</p>
+    </div>
+  );
+}
+
+export default App;

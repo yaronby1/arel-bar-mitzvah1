44172c550d9faa8a1933a9a791b1ee7c3bbbd365
Yaron Ben Yosef. <Yaronby8@gmail.com>
2025-07-11 03:10:24 +0300

    שינויים: הקטנת תמונה, שני כפתורים למרכז

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      {/* תמונה מתוך תיקיית public */}
+      {/* כפתור ראשון */}
+      <div className="center">
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSetOdblPGs4B4DVz20TAmqbw_eaCvZ97BxHnN0aNEaeCMNrMA/viewform?usp=dialog"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="submit">לטופס אישור הגעה</button>
+        </a>
+      </div>
+
+      {/* תמונה מוקטנת */}
       <img src="/invite.jpg" alt="הזמנה" className="invite-image" />
 
-      {/* כפתור שמפנה לטופס גוגל */}
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSetOdblPGs4B4DVz20TAmqbw_eaCvZ97BxHnN0aNEaeCMNrMA/viewform?usp=dialog"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <button className="submit">לטופס אישור הגעה</button>
-      </a>
+      {/* כפתור שני */}
+      <div className="center">
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSetOdblPGs4B4DVz20TAmqbw_eaCvZ97BxHnN0aNEaeCMNrMA/viewform?usp=dialog"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="submit">לטופס אישור הגעה</button>
+        </a>
+      </div>
     </div>
   );
 }
 
 export default App;
-

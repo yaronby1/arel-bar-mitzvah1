ad7639007c4cc81364b4f7449429f03424268615
Yaron Ben Yosef. <Yaronby8@gmail.com>
2025-07-11 04:04:54 +0300

    עודכן לינק לוויז והוסרו כפתורים מיותרים

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [submitted, setSubmitted] = useState(false);
+
   return (
     <div className="container">
+      {/* תמונה מתוך תיקיית public */}
+      <img src="/invite.jpg" alt="הזמנה" className="invite-image" />
+
       {/* כפתורים עליונים */}
       <div className="top-buttons">
         <a
@@ -10,35 +16,35 @@ function App() {
           target="_blank"
           rel="noreferrer"
         >
-          <button className="action-button">לטופס אישור הגעה</button>
+          <button className="submit">לטופס אישור הגעה</button>
         </a>
         <a
-          href="https://waze.com/ul?ll=31.7977,35.1042&navigate=yes"
+          href="https://waze.com/ul?ll=31.794639,35.088424&navigate=yes"
           target="_blank"
           rel="noreferrer"
         >
-          <button className="action-button">הוראות הגעה בוויז</button>
+          <button className="submit">הוראות הגעה בוויז</button>
         </a>
       </div>
 
-      {/* הזמנה */}
-      <img src="/invite.jpg" alt="הזמנה" className="invite-image" />
+      {/* תודה לאחר שליחה */}
+      {submitted && <p className="thank-you">תודה! ההזמנה התקבלה.</p>}
 
       {/* כפתורים תחתונים */}
       <div className="bottom-buttons">
         <a
-          href="https://forms.gle/xxxxxxxxxxxx"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSetOdblPGs4B4DVz20TAmqbw_eaCvZ97BxHnN0aNEaeCMNrMA/viewform?usp=dialog"
           target="_blank"
           rel="noreferrer"
         >
-          <button className="action-button">לטופס אישור הגעה</button>
+          <button className="submit">לטופס אישור הגעה</button>
         </a>
         <a
-          href="https://waze.com/ul?ll=31.795178369828992, 35.068348673134174&navigate=yes"
+          href="https://waze.com/ul?ll=31.794639,35.088424&navigate=yes"
           target="_blank"
           rel="noreferrer"
         >
-          <button className="action-button">הוראות הגעה בוויז</button>
+          <button className="submit">הוראות הגעה בוויז</button>
         </a>
       </div>
     </div>
